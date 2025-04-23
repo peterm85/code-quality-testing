1dd7e7c06673de6e99ae5775f78aa82ad7e09cdb
peterm85 <petermichael85@gmail.com>
2025-04-23 08:59:56 +0200

    Adding UTs

--- src/main/java/com/example/code/application/dto/UserModificationDto.java
+++ src/main/java/com/example/code/application/dto/UserModificationDto.java
@@ -1,10 +1,12 @@
 package com.example.code.application.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class UserModificationDto {
 
   private String name;
--- src/test/java/com/example/code/domain/model/UserTest.java
+++ src/test/java/com/example/code/domain/model/UserTest.java
@@ -4,8 +4,13 @@ import static com.example.code.utils.UserGenerator.createUserCreationDto;
 import static com.example.code.utils.UserGenerator.createUserModificationDto;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import com.example.code.application.dto.UserModificationDto;
 import com.example.code.utils.UserGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class UserTest {
 
@@ -38,5 +43,44 @@ class UserTest {
     assertThat(user.getAddress()).isEqualTo(userModificationDto.getAddress());
     assertThat(user.getCity()).isEqualTo(userModificationDto.getCity());
     assertThat(user.getEmail()).isEqualTo(userModificationDto.getEmail());
+  }
+
+  static Stream<UserModificationDto> providePartialModificationDtos() {
+    return Stream.of(
+            UserModificationDto.builder().name("NewName").build(),
+            UserModificationDto.builder().surname("NewSurname").build(),
+            UserModificationDto.builder().address("NewAddress").build(),
+            UserModificationDto.builder().city("NewCity").build(),
+            UserModificationDto.builder().email("NewEmail").build()
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("providePartialModificationDtos")
+  void modifyUserWithSingleAttribute(UserModificationDto modificationDto) {
+    // Given
+    final var user = UserGenerator.createUser();
+    // When
+    user.modify(modificationDto);
+    // Then
+    checkUserAttributes(user, modificationDto);
+  }
+
+  private void checkUserAttributes(User user, UserModificationDto modificationDto) {
+    if (modificationDto.getName() != null) {
+      assertThat(user.getName()).isEqualTo(modificationDto.getName());
+    }
+    if (modificationDto.getSurname() != null) {
+      assertThat(user.getSurname()).isEqualTo(modificationDto.getSurname());
+    }
+    if (modificationDto.getAddress() != null) {
+      assertThat(user.getAddress()).isEqualTo(modificationDto.getAddress());
+    }
+    if (modificationDto.getCity() != null) {
+      assertThat(user.getCity()).isEqualTo(modificationDto.getCity());
+    }
+    if (modificationDto.getEmail() != null) {
+      assertThat(user.getEmail()).isEqualTo(modificationDto.getEmail());
+    }
   }
 }
